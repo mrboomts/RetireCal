ef98d8edd1acbb4b1c8cc370070623b48ca3f4c9
mrboomts <84792462+mrboomts@users.noreply.github.com>
2025-09-29 09:49:04 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.querySelectorAll('.card').forEach(card => {
-  card.addEventListener('dragstart', e => {
-    card.classList.add('dragging');
-  });
-  card.addEventListener('dragend', e => {
-    card.classList.remove('dragging');
-  });
-});
